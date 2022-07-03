efbe48ba99070f83468d27ee8c067586866d6609
Denis Shashkov <dshashkov@beget.ru>
2022-07-03 22:35:33 +0300

    curler

--- cmd/app.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "github.com/sirupsen/logrus"
-
-func main() {
-	if err := app(); err != nil {
-		logrus.WithError(err).Fatal("application failed with error")
-	}
-}
--- internal/config/config.go
+++ internal/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"github.com/jinzhu/configor"
 	"github.com/pkg/errors"
+	"os"
 )
 
 type RequestList struct {
@@ -11,14 +12,15 @@ type RequestList struct {
 
 type Request struct {
 	Verb string `json:"verb"`
-	Url  string `json:"url"`
-	Urn  string `json:"Urn"`
+	Uri  string `json:"uri"`
 }
 
 func Load(filename string) (RequestList, error) {
 	var list RequestList
+	path, _ := os.Getwd()
+	println(path)
 	if err := configor.Load(&list, filename); err != nil {
-		return RequestList{}, errors.WithMessage(err, "failed to load digList")
+		return RequestList{}, errors.WithMessage(err, "failed to load requestList")
 	}
 	return list, nil
 }
